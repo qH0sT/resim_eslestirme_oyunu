8de611ccf104f59473f0893abff3a8fa6f9ff7b4
qH0sT <asuslaptop230@gmail.com>
2019-08-30 22:37:55 +0300

    Resim eslestirme oyunu

---
+++ ResimEslestirmeOyunu/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ResimEslestirmeOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(661, 520);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(778, 514);
+            this.panel1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11"});
+            this.listBox1.Location = new System.Drawing.Point(914, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 212);
+            this.listBox1.TabIndex = 5;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 520);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Coded By 20071999";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(366, 520);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Puan 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 549);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resim Eşleştirme Oyunu | C#.NET";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ResimEslestirmeOyunu/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ResimEslestirmeOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        List<PictureBox> pcbx_list = new List<PictureBox>();
+        string isim_tutucu = "";
+        int sinir = 0;
+        int puan = 0;
+        int i = 0;
+        private void pic_click(object pic_Box, EventArgs args)
+        {
+            timer1.Stop();
+            if (sinir < 2) //üst üste 2 den fazla basmayı engellemek için.
+            {
+                PictureBox pBoxy = pic_Box as PictureBox;
+
+                try
+                {
+                    using (FileStream fs_im = new FileStream(pBoxy.Tag.ToString(), FileMode.Open, FileAccess.Read))
+                    {
+                        pBoxy.Image = Image.FromStream(fs_im);
+                    }
+
+                }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+                if (isim_tutucu != string.Empty)
+                {
+                    if (pBoxy.Name != isim_tutucu)
+                    {
+                        isim_tutucu = string.Empty;
+                    }
+                    else
+                    {
+                        puan += 1;
+                        label2.Text = "Puan " + puan.ToString();
+                        MessageBox.Show("TEBRİKLER", "Eşleştirme Başarılı");   
+                                             
+                        IEnumerable<PictureBox> resim = panel1.Controls.Cast<PictureBox>();
+                        var imeyc = from x in resim
+                                    where x.Name == isim_tutucu
+                                    select x;
+                        foreach (var f in imeyc)
+                        {
+                            f.Visible = false;
+                        }
+
+                        isim_tutucu = string.Empty;
+                    }
+                }
+                else
+                {
+                    isim_tutucu = pBoxy.Name;
+                }
+
+                sinir += 1;
+            }
+            timer1.Start();
+        }
+        //95, 88
+        bool tekrar_baslat = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (tekrar_baslat == false)
+            {
+                OyunuBaslat();
+                button1.Text = "Tekrar Başlat";
+                tekrar_baslat = true;
+            }
+            else
+            {
+                Application.Restart();
+            }
+           
+        }
+        private void OyunuBaslat()
+        {
+           
+            while (i < 12)
+            {
+                if (pcbx_list.Count == 0)
+                {
+                    PictureBox pcb = new PictureBox();
+                    pcb.Name = "Resim_" + i.ToString();
+                    pcb.Tag = "X_" + i.ToString();
+                    pcb.BackColor = Color.Blue;
+                    pcb.Click += new EventHandler(pic_click);
+                    pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pcb.Left = 12;
+                    pcb.Top = 12;
+                    pcb.Size = new Size(185, 156);
+                    panel1.Controls.Add(pcb);
+                    pcbx_list.Add(pcb);
+                }
+                else
+                {
+                    PictureBox pcb = new PictureBox();
+                    pcb.Name = "Resim_" + i.ToString();
+                    pcb.Click += new EventHandler(pic_click);
+                    pcb.BackColor = Color.Blue;
+                    pcb.Tag = "X_" + i.ToString();
+                    pcb.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pcb.Size = new Size(185, 156);
+                    panel1.Controls.Add(pcb);
+                    pcbx_list.Add(pcb);
+                    if (i % 4 == 0)
+                    {
+                        pcb.Top = pcbx_list[i - 4].Top + 160;
+                        pcb.Left = pcbx_list[i - 4].Left;
+                    }
+                    else
+                    {
+                        pcb.Top = pcbx_list[i - 1].Top;
+                        pcb.Left = pcbx_list[i - 1].Left + 190;
+                    }
+
+                }
+                i = i + 1;
+            }
+            ResimleriDiz();
+        }
+        private void ResimleriDiz()
+        {
+            int say = listBox1.Items.Count - 1;
+            string[] resimler = { "Tavşan", "Ayı", "Kedi", "Köpek", "Arı", "Tavuk" };
+
+            for (int h = 0; h < resimler.Length; h++)
+            {
+                for (int m = 0; m < 2; m++)
+                {
+                    Random rndm = new Random();
+                    int c = Convert.ToInt32(listBox1.Items[rndm.Next(say)]);
+                    panel1.Controls[c].Tag = Environment.CurrentDirectory + "\\Resimler\\" + resimler[h] + ".jpg";
+                    panel1.Controls[c].Name = resimler[h];
+                    //((PictureBox)(panel1.Controls[c])).ImageLocation = Environment.CurrentDirectory + "\\Resimler\\" + resimler[h] + ".jpg";
+                    foreach (var b in listBox1.Items.Cast<object>().ToList())
+                    {
+                        if (c.ToString() == b.ToString())
+                        {
+                            listBox1.Items.Remove(b);
+                        }
+                    }
+                    say = listBox1.Items.Count - 1;
+
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            isim_tutucu = "";
+            sinir = 0;
+            foreach (PictureBox pb in panel1.Controls)
+            {
+                pb.Image = null;
+            }
+        }
+    }
+}
